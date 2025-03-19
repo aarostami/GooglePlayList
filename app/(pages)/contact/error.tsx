36125794d9d35ac6f709dbc63ac7130c2fc6e3af
aarostami <alireza.ro76@gmail.com>
2025-03-19 18:06:59 +0330

    commit

--- app/(pages)/contact/error.tsx
+++ app/(pages)/contact/error.tsx
@@ -1,1 +1,5 @@
 'use client'	// Error boundaries must be Client Components
+
+export default function error() {
+	return <></>
+}
--- app/(pages)/contact/layout.tsx
+++ app/(pages)/contact/layout.tsx
@@ -2,3 +2,6 @@
 /* export default function Layout({ children }: { children: React.ReactNode }) {
 	return <div>{children}</div>
 } */
+export default function layout() {
+	return <></>
+}
